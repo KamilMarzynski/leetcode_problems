52f852cbd418a4d7a55860cbb80c51449275babd
Kamil Marzyński <33965553+KamilMarzynski@users.noreply.github.com>
2026-01-13 09:37:51 +0100

    Time: 40 ms (1.89%) | Memory: 59.6 MB (5.28%) - LeetSync

---
+++ 100-same-tree/same-tree.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    const traverse = function*(node, side = 'root') {
+        if(!node) {
+            return;
+        }
+        yield [node.val, side];
+
+        if(node.left) {
+            yield* traverse(node.left, 'left');
+        } else {
+            yield [null, 'left']
+            yield* traverse(node.right, 'right');
+        }
+        if (node.right) {
+            yield* traverse(node.right, 'right');
+        }
+    }
+
+    const qTraverse = traverse(q);
+    const pTraverse = traverse(p);
+    
+    for (const [value, side] of pTraverse) {
+        console.log('---')
+        console.log('pvalue', value);
+        console.log('pside', side);
+        const next = qTraverse.next();
+        if(next.done) {
+            return false;
+        }
+        
+        const [qvalue, qside] = next.value;
+        console.log('qvalue', qvalue)
+        console.log('qside', qside)
+        console.log('---')
+        if(qvalue !== value || qside !== side) {
+            return false;
+        }
+    }
+
+    return qTraverse.next().done && pTraverse.next().done;
+};
